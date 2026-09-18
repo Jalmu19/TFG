4a68fc503a9aa37f64b81ce48a1b2a0dc1a67aa3
Almudena García Navas <almude12@ucm.es>
2026-09-18 16:43:45 +0200

    Tras ejecucion

--- AnalisisLexico/src/codigo/Main.java
+++ AnalisisLexico/src/codigo/Main.java
@@ -2,9 +2,21 @@ package codigo;
 
 import jflex.exceptions.SilentExit;
 
+import java.io.*;
+
 public class Main {
-    static void main(String[] args) throws SilentExit {
-        String[] ruta = {"C:/Users/almud/OneDrive/Documentos/GitHub/TFG/AnalisisLexico/src/codigo/lexer.l"};
-        jflex.Main.generate(ruta);
+
+    static void main(String[] args) throws SilentExit, IOException {
+        //String[] ruta = {"C:/Users/almud/OneDrive/Documentos/GitHub/TFG/AnalisisLexico/src/codigo/lexer.l"};
+        //jflex.Main.generate(ruta);
+
+        Reader input = new InputStreamReader(new FileInputStream("C:/Users/almud/OneDrive/Documentos/GitHub/TFG/AnalisisLexico/src/input.txt"));
+        AnalizadorLexicoTiny al = new AnalizadorLexicoTiny(input);
+        UnidadLexica unidad;
+        do {
+            unidad = al.yylex();
+            System.out.println(unidad);
+        }
+        while (unidad.clase() != ClaseLexica.EOF);
     }
 }
